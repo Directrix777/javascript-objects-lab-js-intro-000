ddd65271e52b9e7c88c9b01e99cb2180b6d96097
mystical-process-8698 <andrew.j.ribas@gmail.com>
2020-04-27 17:34:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipies
 
 function destructivelyupdateObjectWithKeyAndValue(object, key, value)
 {
-  object.assign(key, value)
+  object = Object.assign(key, value)
 }
 
 function updateObjectWithKeyAndValue(object, key, value)
