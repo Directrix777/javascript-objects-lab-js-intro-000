18fbad568f848f6b2cc5ecd430ae57b5852ab2e4
mystical-process-8698 <andrew.j.ribas@gmail.com>
2020-04-27 18:01:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function updateObjectWithKeyAndValue(object, key, value)
 
 function destructivelyDeleteFromObjectByKey(object, key)
 {
-  delete object.key
+  delete object.[key]
   return object
 }
 
